236237f8be3bae2f4597950f24c94019f941d566
saophaidihoc <saophaidihoc@gmail.com>
2025-11-28 21:13:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I like you too";
+  question.innerHTML = "Awwwww, mà Đức thì hơn thế cơ vì Đức yêu Linh lắm ó!!!!!";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
